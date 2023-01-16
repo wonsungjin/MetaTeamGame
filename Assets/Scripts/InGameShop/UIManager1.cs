37409f4f6128e318230c8521707b68fc4800e745
woo <coinus@naver.com>
2023-01-16 14:38:58 +0900

    timerFix

--- Assets/Scripts/InGameShop/UIManager1.cs
+++ Assets/Scripts/InGameShop/UIManager1.cs
@@ -6,6 +6,8 @@ public partial class UIManager : MonoBehaviour
 {
     private static UIManager instance = null;
 
+    [Header("슬라이더")]
+    [SerializeField] Slider timerSlider = null;
     [Header("버튼")]
     public Button ReFreshButton = null;
     public Button LevelUpButton = null;
@@ -13,13 +15,14 @@ public partial class UIManager : MonoBehaviour
     [SerializeField] Text NowShopLevelTXT = null;
     [SerializeField] Text goldTXT = null;
     public Text shopLevelTXT = null;
-    [SerializeField] Text sellTxt = null;
-
+    [SerializeField] Text sellTXT = null;
+    [SerializeField] Text timerTXT = null;
     [SerializeField] public GameObject sell = null;
 
     public int shopMoney = 0;
     public int goldCount = 10;
     public int shopLevel = 1;
+    float timer = 60f;
 
     // Public 프로퍼티로 선언해서 외부에서 private 멤버변수에 접근만 가능하게 구현
     public static UIManager Instance
@@ -54,25 +57,29 @@ public partial class UIManager : MonoBehaviour
 
     private void Update()
     {
+        timer -= Time.deltaTime;
+        timerTXT.text = string.Format("Timer : {0:N0}", timer);
+        timerSlider.value = timer;
+        
+
+
         goldTXT.text = "Gold : " + goldCount.ToString();
         shopLevelTXT.text = "Shop Gold :" + shopMoney.ToString();
         NowShopLevelTXT.text = "Shop Level :" + shopLevel.ToString();
 
         if(sell.activeSelf == false)
         {
-            sellTxt.gameObject.SetActive(false);
+            sellTXT.gameObject.SetActive(false);
         }
         else
         {
-            sellTxt.gameObject.SetActive(true);
+            sellTXT.gameObject.SetActive(true);
         }
     }
-
-
 
     private void FirstReset()
     {
         sell.gameObject.SetActive(false);
-        sellTxt.gameObject.SetActive(false);
+        sellTXT.gameObject.SetActive(false);
     }
 }
