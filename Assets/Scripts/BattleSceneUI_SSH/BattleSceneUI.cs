fafe6df964d543aba57e8df855de7091d25d10a8
Lucid-Kim <97496901+kory199@users.noreply.github.com>
2023-01-19 17:45:39 +0900

    Update BattleSceneUI.cs

--- Assets/Scripts/BattleSceneUI_SSH/BattleSceneUI.cs
+++ Assets/Scripts/BattleSceneUI_SSH/BattleSceneUI.cs
@@ -16,7 +16,7 @@ public class BattleSceneUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+                    
     }
 
     public void OnClickOptionButton()
