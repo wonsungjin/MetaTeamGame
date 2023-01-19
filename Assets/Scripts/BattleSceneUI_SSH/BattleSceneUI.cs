7b0d6f5f4de7767cc9a2da00767606e657eaa4cf
Lucid-Kim <97496901+kory199@users.noreply.github.com>
2023-01-19 11:31:49 +0900

    adding BattleScene UI

---
+++ Assets/Scripts/BattleSceneUI_SSH/BattleSceneUI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickOptionButton()
+    {
+
+    }
+
+    public void OnClickProfilButton()
+    {
+        if (gameObject.name.CompareTo("PlayerProfilButton") == 0)
+        {
+            
+        }
+    }
+}
