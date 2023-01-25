8582061652f89e4a07f7b987b93063045d715687
mytortoise <69615078+HookHookUK@users.noreply.github.com>
2023-01-25 18:33:47 +0900

    batch fix working

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -61,26 +61,31 @@ public class Skill : MonoBehaviour
     }
     public virtual void Skill_TrunEnd() // 턴 종료시
     {
-        int gold = 0;
-        int buffHp = 0;
+
     }
 
     //================== 세부 스킬 내용 (스킬 함수 내부에서 호출되는 함수들)=================================================
 
     public void GetGold(int value) // 돈 획득
     {
-
+        // 상점 씬으로 넘어가서 
+        GameMGR.Instance.uiManager.goldCount += value;
     }
-    public void Target(int who, int whom)  // 타겟팅 - 누가, 누구에게
+    public void Target(Card who, Card whom)  // 타겟팅 - 누가, 누구에게
     {
 
     }
-    public void Summon(GameObject summon) // 소환
+    public void Summon(GameObject summoned) // 소환
+    {
+        // 자신의 필드에 빈 칸이 있는지 확인
+
+        // 빈 칸이 있다면 해당 위치에 소환(빈칸 중 인덱스 순서대로 우선순위)
+
+    }
+    public void Buff(Card card,string key, bool isTemp) // 버프 - 버프 대상, 버프 키워드(공격력/체력/경험치), 일시적 여부
     {
 
     }
-    public void Buff(Card card,string key) // 버프
-    {
 
-    }
+
 }
