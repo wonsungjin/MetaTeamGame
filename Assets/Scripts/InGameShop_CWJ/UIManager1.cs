f605325ac2fb3b173f84b2c2d22dd150e6739ca5
woo <coinus@naver.com>
2023-01-26 15:57:39 +0900

    Revert "ComitPrefab"
    This reverts commit a38a13bec466560d6638bf7c6812a5e6d9dc5fe3.

--- Assets/Scripts/InGameShop_CWJ/UIManager1.cs
+++ Assets/Scripts/InGameShop_CWJ/UIManager1.cs
@@ -63,7 +63,7 @@ public partial class UIManager : MonoBehaviour
             // 시간이 변경한 만큼 slider Value 변경을 합니다.
             timer -= Time.deltaTime;
             timerSlider.value = timer;
-            timerTXT.text = string.Format("Time : {0:N0}sec" , timer);
+            timerTXT.text = string.Format("Timer : {0:N0}", timer);
 
             goldTXT.text = "" + goldCount.ToString();
             shopLevelTXT.text = "Shop Gold :" + shopMoney.ToString();
