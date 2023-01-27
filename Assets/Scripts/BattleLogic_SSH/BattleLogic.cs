e1997596c4de95ddccbc69cfdfe33598b1efd691
mytortoise <69615078+HookHookUK@users.noreply.github.com>
2023-01-27 14:08:53 +0900

    d

--- Assets/Scripts/BattleLogic_SSH/BattleLogic.cs
+++ Assets/Scripts/BattleLogic_SSH/BattleLogic.cs
@@ -20,7 +20,7 @@ public partial class BattleLogic : MonoBehaviourPunCallbacks
 
     private int playerTurnCount = 0; // Player Turn Count
     private int enemyTurnCount = 0; // Enemy Turn Count
-    private int randomArrayNum = 0;
+    private int randomArrayNum = 0; 
 
     // 추후 master client가 gamemananger에서 생성한 랜덤 배열로 대체 예정 (매 라운드 생성 및 배포)
     private int[] exArray = new int[100];
