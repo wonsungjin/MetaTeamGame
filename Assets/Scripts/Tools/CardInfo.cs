c63f28a1b048a9445786ce28aca100291e127f58
wonsj <wonsj09@naver.com>
2023-02-08 17:18:32 +0900

    ww
    ww

--- Assets/Scripts/Tools/CardInfo.cs
+++ Assets/Scripts/Tools/CardInfo.cs
@@ -96,15 +96,15 @@ public class CardInfo : ScriptableObject
     [SerializeField] internal SkillTiming skillTiming;
     [SerializeField] internal TargetType targetType;
     [SerializeField] internal EffectTarget effectTarget;
-    [SerializeField] internal int minTarget;
-    [SerializeField] internal int maxTarget;
+    [SerializeField] internal string minTarget;
+    [SerializeField] internal string maxTarget;
     [SerializeField] internal TriggerCondition triggerCondition;
     [SerializeField] internal EffectType effectType;
-    [SerializeField] internal int value1;
-    [SerializeField] internal int value2;
-    [SerializeField] internal int value3;
+    [SerializeField] internal string value1;
+    [SerializeField] internal string value2;
+    [SerializeField] internal string value3;
     [SerializeField] internal string sumom_Unit;
-    [SerializeField] internal int num_Triggers;
+    [SerializeField] internal string num_Triggers;
     [SerializeField] internal int duration;
     [SerializeField] internal string appear;
 
@@ -126,6 +126,77 @@ public class CardInfo : ScriptableObject
             return $"Lv 3:{skillExplantion[2]}";
         }
         return null;
+    }
+    public int GetMaxTarget(int level)
+    {
+        string[] Target = maxTarget.Split(".");
+        if (level == 1)
+        {
+            return int.Parse(Target[0]);
+        }
+        else if (level == 2)
+        {
+            if (Target.Length == 1) return int.Parse(Target[0]);
+            return int.Parse(Target[1]);
+        }
+        else if (level == 3)
+        {
+            if (Target.Length == 1) return int.Parse(Target[0]);
+            return int.Parse(Target[2]);
+        }
+        return 0;
+
+    }
+    public int GetNumTrigger(int level)
+    {
+        string[] numTrigger = num_Triggers.Split(".");
+        if (level == 1)
+        {
+            return int.Parse(numTrigger[0]);
+        }
+        else if (level == 2)
+        {
+            if (numTrigger.Length == 1) return int.Parse(numTrigger[0]);
+            return int.Parse(numTrigger[1]);
+        }
+        else if (level == 3)
+        {
+            if (numTrigger.Length == 1) return int.Parse(numTrigger[0]);
+            return int.Parse(numTrigger[2]);
+        }
+        return 0;
+
+    }
+    public int GetValue(int valueNum, int level)
+    {
+        string[] value=null;
+        if (valueNum == 1)
+        {
+            value = value1.Split(".");
+        }
+        else if (valueNum == 1)
+        {
+            value = value2.Split(".");
+        }
+        else
+        {
+            value = value3.Split(".");
+        }
+        if (level == 1)
+        {
+            return int.Parse(value[0]);
+        }
+        else if (level == 2)
+        {
+            if (value.Length == 1) return int.Parse(value[0]);
+            return int.Parse(value[1]);
+        }
+        else if (level == 3)
+        {
+            if (value.Length == 1) return int.Parse(value[0]);
+            return int.Parse(value[2]);
+        }
+        return 0;
     }
 
 }
