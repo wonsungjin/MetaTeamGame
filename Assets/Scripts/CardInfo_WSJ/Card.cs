e4c09fdfa98069021a3a5f83609150b243df48d6
wonsj <wonsj09@naver.com>
2023-02-10 10:19:56 +0900

    www
    www

--- Assets/Scripts/CardInfo_WSJ/Card.cs
+++ Assets/Scripts/CardInfo_WSJ/Card.cs
@@ -27,7 +27,7 @@ public partial class Card : MonoBehaviour
     private void Awake()
     {
         SetMyInfo(name);
-        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource = gameObject.GetComponent<AudioSource>();        
     }
 
     /*자신의 오브젝트 이름과 같은 스크립터블 데이터를 읽어와서 설정한다
