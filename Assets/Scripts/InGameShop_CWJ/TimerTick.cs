99c8d59af2ec8a0e546391d272c73a64ba9bbd30
woo <coinus@naver.com>
2023-02-20 12:28:41 +0900

    TimerFix

--- Assets/Scripts/InGameShop_CWJ/TimerTick.cs
+++ Assets/Scripts/InGameShop_CWJ/TimerTick.cs
@@ -1,47 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Networking;
-using UnityEngine.UI;
 
 public class TimerTick : MonoBehaviour
 {
-    public Sprite[] sprites; // 이미지 배열
-    public Image image; // 이미지 컴포넌트
-    WaitForSeconds waittimer = new WaitForSeconds(0.3f);
-
-    private int currentSprite = 0; // 현재 이미지 인덱스
+    public float rotateSpeed = 30f;
+    public float rotateAngle = 10f;
+    private float startingAngle;
+    private float targetAngle;
+    private bool rotateRight = true;
 
     void Start()
     {
-        StartCoroutine(AnimateSprite()); // 코루틴 시작
+        startingAngle = transform.eulerAngles.z;
+        targetAngle = startingAngle + rotateAngle;
     }
 
-    IEnumerator AnimateSprite()
+    void Update()
     {
-        while (true)
+        float currentAngle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, targetAngle, rotateSpeed * Time.deltaTime);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentAngle);
+
+        if (currentAngle == targetAngle)
         {
-            image.sprite = sprites[currentSprite]; // 이미지 변경
-            currentSprite++; // 인덱스 증가
-
-            if (currentSprite >= sprites.Length)
+            if (rotateRight)
             {
-                currentSprite = 0; // 배열 끝에 도달하면 처음으로 돌아감
+                targetAngle = startingAngle - rotateAngle;
+            }
+            else
+            {
+                targetAngle = startingAngle + rotateAngle;
             }
 
-            if (GameMGR.Instance.uiManager.isTimerFast == false)
-            {
-                Debug.Log("타이머 슬로우");
-                yield return waittimer; // 0.3초 대기
-            }
-
-            else if (GameMGR.Instance.uiManager.isTimerFast == true)
-            {
-                Debug.Log(GameMGR.Instance.uiManager.isTimerFast);
-                Debug.Log("타이머 패스트");
-                yield return new WaitForSeconds(0.1f);
-            }
+            rotateRight = !rotateRight;
         }
+
+        if (GameMGR.Instance.uiManager.isTimerFast == true)
+        {
+            TimerTickFaster();
+        }
+    }
+
+    void TimerTickFaster()
+    {
+        rotateSpeed = 50f;
     }
 }
