5eff8d2150023fb02e9a3b0ffea48b43176732cf
woo <coinus@naver.com>
2023-02-21 15:09:28 +0900

    UIADD

--- Assets/Scripts/InGameShop_CWJ/Node.cs
+++ Assets/Scripts/InGameShop_CWJ/Node.cs
@@ -46,26 +46,32 @@ public class Node : MonoBehaviour
                 {
                     case 1:
                         stars[0].SetActive(true);
+                        stars[0].gameObject.layer = 2;
                         num = 0;
                         break;
                     case 2:
                         stars[1].SetActive(true);
+                        stars[1].gameObject.layer = 2;
                         num = 1;
                         break;
                     case 3:
                         stars[2].SetActive(true);
+                        stars[2].gameObject.layer = 2;
                         num = 2;
                         break;
                     case 4:
                         stars[3].SetActive(true);
+                        stars[3].gameObject.layer = 2;
                         num = 3;
                         break;
                     case 5:
                         stars[4].SetActive(true);
+                        stars[4].gameObject.layer = 2;
                         num = 4;
                         break;
                     case 6:
                         stars[5].SetActive(true);
+                        stars[5].gameObject.layer = 2;
                         num = 5;
                         break;
                 }
