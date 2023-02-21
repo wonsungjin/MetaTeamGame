899f8fa065c5a9cd2c268be3bf6bc9c211f06c5e
SSH <kory199@Gmail.com>
2023-02-21 13:46:58 +0900

    none

---
+++ Assets/Scripts/TESTSpine.cs
@@ -0,0 +1,13 @@
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTSpine : MonoBehaviour
+{
+    public AnimationReferenceAsset blink;
+    public float mini = 0.15f;
+    public float max = 3f;
+
+
+}
