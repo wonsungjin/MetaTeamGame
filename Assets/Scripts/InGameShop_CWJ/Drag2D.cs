d8a012b1b967a9f66b6e13a65cd289b3a969ed4e
woo <coinus@naver.com>
2023-02-22 18:28:43 +0900

    TransFormFix

--- Assets/Scripts/InGameShop_CWJ/Drag2D.cs
+++ Assets/Scripts/InGameShop_CWJ/Drag2D.cs
@@ -19,6 +19,7 @@ public partial class Drag2D : MonoBehaviour
     Vector3 monsterPos1 = new Vector3(0, 0.6f, 0);
 
     Vector3 vecs = new Vector3(0, -0.6f, 1f);
+    Vector3 vecs1 = new Vector3(0, 1.4f, 1f);
 
     float timer = 0f;
     float distance = 100;
@@ -68,7 +69,7 @@ public partial class Drag2D : MonoBehaviour
                         if (timer > 1f)
                         {
                             GameObject vec = GameObject.FindGameObjectWithTag("BattleZone");
-                            hit.collider.gameObject.transform.parent.position = vec.transform.position + Vector3.down;
+                            hit.collider.gameObject.transform.parent.position = vec.transform.position + Vector3.down + new Vector3(0,-0.4f);
                             vec.GetComponent<BattleZone>().myObj = hit.collider.gameObject.transform.parent.gameObject;
                         }
                     }
@@ -300,6 +301,8 @@ public partial class Drag2D : MonoBehaviour
     {
         yield return wait;
 
+        Debug.Log("아넝훙낳");
+
         if (CompareTag("BattleMonster") || CompareTag("BattleMonster2") || CompareTag("BattleMonster3"))
         {
             if (pos.myObj != null)
@@ -310,7 +313,7 @@ public partial class Drag2D : MonoBehaviour
             }
             else
             {
-                this.transform.parent.position = pos.gameObject.transform.position + Vector3.down;
+                this.transform.parent.position = pos.gameObject.transform.position - vecs1;
                 pos.myObj = gameObject.transform.parent.gameObject;
             }
         }
