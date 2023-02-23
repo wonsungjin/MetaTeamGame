3538f25aacc56ed324529fd28f1af68d900d7281
woo <coinus@naver.com>
2023-02-23 17:06:24 +0900

    SpecialZoneFix

--- Assets/Scripts/InGameShop_CWJ/SellInCollider.cs
+++ Assets/Scripts/InGameShop_CWJ/SellInCollider.cs
@@ -9,14 +9,23 @@ public class SellInCollider : MonoBehaviour
     [SerializeField] GameObject[] specialZone = null;
     Collider2D[] specialColl = null;
 
+    [SerializeField] GameObject[] nomalZone = null;
+    Collider2D[] nomalColl = null;
+
 
     private void Start()
     {
         specialColl = new Collider2D[specialZone.Length];
+        nomalColl = new Collider2D[nomalZone.Length];
 
         for (int i = 0; i < 2; i++)
         {
             specialColl[i] = specialZone[i].GetComponent<Collider2D>();
+        }
+
+        for (int i = 0; i < nomalZone.Length; i++)
+        {
+            nomalColl[i] = nomalZone[i].GetComponent<Collider2D>(); 
         }
 
         for (int i = 0; i < 2; i++)
@@ -29,20 +38,44 @@ public class SellInCollider : MonoBehaviour
     {
         if (sell.activeSelf == true)
         {
-            Debug.Log("현재 셀버튼이 트루다");
             for (int i = 0; i < 2; i++)
             {
                 specialColl[i].enabled = false;
+            }
+
+            for (int i = 0; i < nomalZone.Length; i++)
+            {
+                nomalColl[i].enabled = false;
             }
         }
 
         if (sell.activeSelf == false)
         {
-            Debug.Log("현재 셀버튼이 폴스다");
             for (int i = 0; i < 2; i++)
             {
                 specialColl[i].enabled = true;
             }
+
+            for (int i = 0; i < nomalZone.Length; i++)
+            {
+                nomalColl[i].enabled = true;
+            }
+        }
+    }
+
+    public void CollOn()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            specialColl[i].enabled = true;
+        }
+    }
+
+    public void NomalCollOn()
+    {
+        for (int i = 0; i < nomalZone.Length; i++)
+        {
+            nomalColl[i].enabled = true;
         }
     }
 }
