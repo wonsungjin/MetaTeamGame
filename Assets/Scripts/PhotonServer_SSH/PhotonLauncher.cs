51b8a80e184bb1954abdbbc6ec281e6cacd85a03
mytortoise <69615078+HookHookUK@users.noreply.github.com>
2023-02-27 21:26:00 +0900

    ㅇㅁㄹㅇㅁㄴㄹㄴㅁ

--- Assets/Scripts/PhotonServer_SSH/PhotonLauncher.cs
+++ Assets/Scripts/PhotonServer_SSH/PhotonLauncher.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 
 public class PhotonLauncher : MonoBehaviourPunCallbacks
 {
-    byte maxPlayer = 3;
+    byte maxPlayer = 4;
 
     [SerializeField] TextMeshProUGUI playerCount = null;
 
